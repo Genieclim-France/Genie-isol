662b6e5a14f25b5aade302ba886009b4631c96e7
Axel <axeljpro@gmail.com>
2025-09-08 15:56:17 +0200

    fix

---
+++ src/app/components/home/Slider.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import VideoSlider from "./Slider-video";
+
+function Slider() {
+  return (
+    <section className="flex flex-col p-6 pt-16 xl:px-20 2xl:px-52">
+      <h4 className="text-xl mb-10 font-emOne uppercase text-nowrap after:content-[''] after:mt-2 after:block after:w-10 after:h-[0.35rem] after:bg-primary lg:text-2xl lg:px-[8.5rem]">
+        Ils nous ont fait confiance !
+      </h4>
+      <div className="md:px-32">
+        <VideoSlider />
+      </div>
+    </section>
+  );
+}
+export default Slider;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import BannerMid from "./components/home/BannerMid";
 import Hero from "./components/home/Hero";
 import FormDevis from "./components/home/FormDevis";
 import Products from "./components/home/Products";
+import BannerDown from "./components/home/BannerDown";
+import Slider from "./components/home/Slider";
 
 export default function Home() {
   return (
@@ -11,7 +13,31 @@ export default function Home() {
       <Article />
       <BannerMid />
       <Products />
+      <BannerDown />
+      <Slider />
       <FormDevis />
+      <section className="flex flex-col p-8 lg:px-32 py-20">
+        <h3 className="text-2xl font-bold after:content-[''] after:block after:w-10 after:h-[0.35rem] after:bg-primary after:mt-4">
+          Un accompagnement 100% personnalisée à chaque projet
+        </h3>
+        <p className="py-4 text-base font-kollektif">
+          Grâce à des équipes qualifiées et expérimentées, nous répondons aux
+          besoins des constructeurs, promoteurs, maîtres d’œuvre, entreprises
+          générales et artisans qui recherchent un partenaire fiable et réactif
+          pour leurs chantiers. Nous sommes certifiés Qualibat RGE, gage de
+          qualité et de conformité aux normes en vigueur. Cela garantit à nos
+          partenaires des prestations soignées, respectant les standards du
+          secteur et répondant aux attentes les plus exigeantes en termes de
+          performance énergétique et de durabilité.
+        </p>
+        <p className="text-base font-kollektif">
+          Notre organisation, pensée pour la sous-traitance, nous permet
+          d’intervenir efficacement sur des chantiers de toute taille, partout
+          en France. Nous mettons un point d’honneur à respecter les délais, les
+          contraintes techniques et les exigences de sécurité propres à chaque
+          projet.
+        </p>
+      </section>
     </>
   );
 }
