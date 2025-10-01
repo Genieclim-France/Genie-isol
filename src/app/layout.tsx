5eeb7c0415ab3dd6b233aae8ab6daf3877f3f1e1
Axel <axeljpro@gmail.com>
2025-10-01 11:07:06 +0200

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 // EmOne variants
 const EmOneSemiBold = localFont({
@@ -63,6 +64,7 @@ export default function RootLayout({
     <html lang="fr">
       <body className={`${EmOneSemiBold.variable} ${Kollektif.variable}`}>
         {/* <Axeptio /> */}
+        <Analytics />
         <Navbar />
         {children}
         <Footer />
